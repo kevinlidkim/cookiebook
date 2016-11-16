6493dd9a98e3582f8107738a08bb5b72e63f4a1f
kevin <kevin.li.3@stonybrook.edu>
2016-11-16 16:04:59 -0500

    converted all the data into one object

--- app/controllers/pages.js
+++ app/controllers/pages.js
@@ -62,8 +62,6 @@ exports.loadPage = function(req, res) {
   db.PostedOn.findAll({ where: {page: req.body.page} })
     .then(function(postRelation) {
 
-      var promiseArray = [];
-
       // Get all the posts
       var promiseArrayPosts = [];
       _.forEach(postRelation, function(getPost) {
@@ -74,54 +72,174 @@ exports.loadPage = function(req, res) {
       Promise.all(promiseArrayPosts).then(values => {
         pageData.arrayOfPosts = values;
 
-        // Find all the comments for each post
+        // Get all the comment relations
+        var promiseArrayComments = [];
         _.forEach(values, function(post) {
-          db.CommentedOn.findAll({ where: {post: values.postId} })
-            .then(function(commentRelation) {
-
-              // Get all the comments
-              var promiseArrayComments = [];
-              _.forEach(commentRelation, function(getComment) {
-                promiseArrayComments.push(db.Post.find({ where: {commentId: getComment.comment} }));
-              })
-
-              // Resolve the promise --> now we have all comments for that post
-              Promise.all(promiseArrayComments).then(values2 => {
-                promiseArray.push(values2);
-                console.log('yoooooo');
-              })
-
-            })       
+          promiseArrayComments.push(db.CommentedOn.findAll({ where: {post: post.postId} }));
         })
 
+        // Resolve the promise --> now we have the comment relations
+        Promise.all(promiseArrayComments).then(commentRelations => {
+          var flattenArray = [].concat.apply([], commentRelations);
+          pageData.postCommentRelation = flattenArray;
+
+          // Get all the comments
+          var superPromiseArray = [];
+          _.forEach(flattenArray, function(comment) {
+            superPromiseArray.push(db.Comment.findAll({ where: {commentId: comment.comment} }))
+          })
+
+          // Resolve the promise --> now we have all the comments
+          Promise.all(superPromiseArray).then(arrayOfComments => {
+            var flattenComments = [].concat.apply([], arrayOfComments);
+            pageData.arrayOfComments = flattenComments;
+          })
+          .then(function() {
+
+            // Convert arrays to maps
+            var comments = _.keyBy(pageData.arrayOfComments, 'commentId');
+            var posts = _.keyBy(pageData.arrayOfPosts, 'postId');
+
+            var arrayFinalData = [];
+            _.forEach(posts, function(post) {
+              var result = {
+                postId: post.postId,
+                content: post.content,
+                likes: post.likes,
+                commentCount: post.commentCount,
+                comments: []
+              }
+              arrayFinalData.push(result);
+            })
+
+            // finalData is where we get all posts and comments
+            var finalData = _.keyBy(arrayFinalData, 'postId');
+
+            // Add all the comments into finalData
+            _.forEach(pageData.postCommentRelation, function(relation) {
+              var postId = relation.post;
+              finalData[postId].comments.push(relation.comment);
+            })
+
+            return res.status(200).json({
+              status: "Retrieved all comments successfully",
+              pageData: pageData,
+              finalData: finalData
+            });
+          })
+          .catch(function(err) {
+            console.log(err);
+            return res.status(500).json({
+              status: "Error retrieving all comments"
+            });
+          })
+
+        })
 
       })
 
-      // Resolve all promises --> now we have all the data we need
-      Promise.all(promiseArray).then(allData => {
-        var x = 0;
-        _.forEach(allData, function(arrayOfData) {
-          pageData.arrayOfPosts[x].comments = arrayOfData;
-          console.log(x);
-          x++;
-        })
-        return pageData;
-      })
-      .then(function(stuff) {
-        return res.status(200).json({
-          status: 'Retrieved all data',
-          pageData: pageData
-        })
-      })
-      .catch(function(err) {
-        return res.status(500).json({
-          status: 'Error retrieving all data'
-        })
-      })
 
     })
 
 }
+
+// exports.loadPage = function(req, res) {
+
+//   var pageData = {
+//     page: req.body.page
+//   }
+
+//   var promiseArray = [];
+//   var x = 0;
+//   var y = 0;
+
+//   // Find all posts on the page
+//   db.PostedOn.findAll({ where: {page: req.body.page} })
+//     .then(function(postRelation) {
+
+//       // Get all the posts
+//       var promiseArrayPosts = [];
+//       _.forEach(postRelation, function(getPost) {
+//         promiseArrayPosts.push(db.Post.find({ where: {postId: getPost.post} }));
+//       })
+
+//       // Resolve the promise --> now we have the posts
+//       Promise.all(promiseArrayPosts).then(values => {
+//         pageData.arrayOfPosts = values;
+
+//         // Find all the comments for each post
+//         var superPromise = _.forEach(values, function(post) {
+//           // I THINK THIS PROMISE SCREWS EVERYTHING UP? THIS GETS THE RELATION TO FIND THE COMMENT
+//           db.CommentedOn.findAll({ where: {post: post.postId} })
+//             .then(function(commentRelation) {
+
+
+//               console.log('wtf yo');
+
+//               // Get all the comments
+//               var promiseArrayComments = [];
+//               _.forEach(commentRelation, function(getComment) {
+//                 console.log('it doesnt even get in here');
+//                 promiseArrayComments.push(db.Comment.find({ where: {commentId: getComment.comment} }));
+//                 // promiseArray.push(db.Comment.find({ where: {commentId: getComment.comment} }));
+//               })
+
+//               superPromise['comment' + x] = promiseArrayComments;
+//               x++;
+
+//               return promiseArrayComments
+
+//             })
+//             // SHOULD PUT A THEN HERE AND DO SOMETHING TO GET THE STUFF CORRECTLY
+//             .then(function(promiseChain) {
+//               console.log('does it get here');
+//               Promise.all(promiseChain).then(function (results) {
+//                 superPromise['comments' + y] = results;
+//                 console.log(superPromise['comments' + y]);
+//               })
+//             })
+//         })
+
+//         // console.log(superPromise);
+
+//         // Resolve all promises --> now we have all the data we need
+//         Promise.all(superPromise)
+//         .then(function(stuff) {
+//           console.log('this should print last?');
+//           // console.log(stuff);
+//           return res.status(200).json({
+//             status: 'Retrieved all data',
+//             pageData: pageData
+//           })
+//         })
+//         .catch(function(err) {
+//           return res.status(500).json({
+//             status: 'Error retrieving all data'
+//           })
+//         })
+
+//       })
+
+//       // // Resolve all promises --> now we have all the data we need
+//       // Promise.all(promiseArray)
+//       // .then(function(stuff) {
+//       //   console.log('uhhhh stuff?');
+//       //   // console.log(stuff);
+//       //   return res.status(200).json({
+//       //     status: 'Retrieved all data',
+//       //     pageData: pageData
+//       //   })
+//       // })
+//       // .catch(function(err) {
+//       //   return res.status(500).json({
+//       //     status: 'Error retrieving all data'
+//       //   })
+//       // })
+
+//     })
+
+// }
+
 
 exports.getPersonalPageId = function(req, res) {
 
